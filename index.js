884b31d05afb763f331fce4d22d9c0267dcd4541
Samer Mobarak <samermobarak888@gmail.com>
2023-08-07 16:00:17 -0400

    arithmetic_lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+//test 1
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+//test 2
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+//test 3
+
+let num3 = 8
+let num4 = 2
+const mod = num3 / num4
+
+//test 4
+
+const numbers = [8, 20, 6, 14, 19]
+const max = Math.max(...numbers);
